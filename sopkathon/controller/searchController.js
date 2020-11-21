c496739709a7508e15ad9f51c6b25db54039ffb3
jaeukyang <didwodnr123@gmail.com>
2020-11-22 07:35:44 +0900

    UPDATE: searchController

--- sopkathon/controller/searchController.js
+++ sopkathon/controller/searchController.js
@@ -14,7 +14,7 @@ module.exports = {
             return res.status(statusCode.OK).send(util.success(statusCode.OK, responseMessage.SEARCH_READ_ALL_SUCCESS, searchData));
         } catch (error) {
             console.error(error);
-            return res.status(statusCode.INTERNAL_SERVER_ERROR).send(util.fail(statusCode.INTERNAL_SERVER_ERROR, responseMessage.SEARCH_READ_ALL_FAIL));
+            return res.status(statusCode.BAD_REQUEST).send(util.fail(statusCode.BAD_REQUEST, responseMessage.SEARCH_READ_ALL_FAIL));
         }
     },
 }
