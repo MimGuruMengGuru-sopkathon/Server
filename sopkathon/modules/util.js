5aefa93d9286ac38d90cac13569c2341a6f9394c
jaeukyang <didwodnr123@gmail.com>
2020-11-22 04:08:17 +0900

    UPDATE: app.js, util.js

--- sopkathon/modules/util.js
+++ sopkathon/modules/util.js
@@ -1,5 +1,4 @@
 module.exports = {
-<<<<<<< HEAD
   success: (status, message, data) => ({
     status,
     success: true,
@@ -12,17 +11,4 @@ module.exports = {
     message,
   }),
 };
-=======
-    success: (status, message, data) => ({
-        status,
-        success: true,
-        message,
-        data,
-    }),
-    fail: (status, message) => ({
-        status,
-        success: false,
-        message,
-    }),
-};
->>>>>>> 0614e79d5a9f83651f579ed44fa549a43d15f484
+
