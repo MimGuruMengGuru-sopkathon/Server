5e786445a07c0bea54cc84614d2939aeea8c608a
jeongjae <rchmas@gmail.com>
2020-11-22 04:05:43 +0900

    fix app

--- sopkathon/app.js
+++ sopkathon/app.js
@@ -6,15 +6,14 @@ var logger = require('morgan');
 
 const { sequelize } = require('./models');
 sequelize.sync({ alter: false })
-.then(() => {
-  console.log('데이터베이스 연결 성공.')
-})
-.catch((error) => {
-  console.error(error);
-});
+  .then(() => {
+    console.log('데이터베이스 연결 성공.')
+  })
+  .catch((error) => {
+    console.error(error);
+  });
 
 var indexRouter = require('./routes/index');
-var usersRouter = require('./routes/posts');
 
 var app = express();
 
@@ -24,15 +23,6 @@ app.set('view engine', 'jade');
 
 app.use(logger('dev'));
 
-const { sequelize } = require('./models');
-sequelize.sync({ alter: false })
-  .then(() => {
-    console.log('데이터베이스 연결 성공.')
-  })
-  .catch((error) => {
-    console.error(error);
-  });
-
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
@@ -40,7 +30,7 @@ app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', indexRouter);
-app.use('/posts', usersRouter);
+
 
 // catch 404 and forward to error handler
 app.use(function (req, res, next) {
